8bca12add6aed8632be8491872e8beba92ad703e
SweetWeeds <khk4502@gmail.com>
2020-08-03 15:17:27 +0000

    20200804_1217

---
+++ turtlebot3_manipulation/turtlebot3_manipulation_navigation/src/node.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+/** 
+ *
+ * 
+ **/
+class node {
+private:
+    int x, y;
+public:
+    node(int x, int y) {
+
+    }
+    void linkNode(node *node) {
+
+    }
+};
+
+class map {
+
+};
